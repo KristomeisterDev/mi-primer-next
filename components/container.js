cf40bd88f37ea2ce5cb905d86d31084b97f2083c
Cristian Luna <cristianluru@gmail.com>
2022-05-03 17:28:03 -0500

    index

---
+++ components/container.js
@@ -0,0 +1,17 @@
+import Navigation from './navigation'
+import Head from 'next/head'
+
+const Container = (props) => {
+    <div>
+        <Head>
+            <title>Next.js Project</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/flatly/bootstrap.min.css"/>
+        </Head>
+        <Navigation/>
+        <div>
+            {props.children}
+        </div>
+    </div>
+}
+
+export default Container
---
+++ pages/services.js
@@ -0,0 +1,11 @@
+import Container from '../components/container'
+const Services = () => {
+    return (
+            <Container>
+                <h1>Services</h1>
+                <p>lorem ipsum dolor sit amet, consectetur adip</p>
+            </Container>
+    )
+}
+
+export default Services
